ba4eb2c9a65ae214bef81463b3c65715a2c75d25
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-28 16:10:19 +0100

    finalizzazione e inserimento commenti js-snack-4

--- js-snack-4/main.js
+++ js-snack-4/main.js
@@ -1,4 +1,4 @@
-
+// creo un array contenente la lista di invitati alla festa
 const invitedList = [
     'matteo', 
     'marco', 
@@ -11,17 +11,21 @@ const invitedList = [
     'claudio',
     'daniela'];
 
+
+// inserisco una sezione di input dove controllare se un nome inserito è presente nella lista invitati
 let inputUserText = prompt("inserisci il tuo nome");
 
 let checkUserInvitation = false;
 
-for (let i=0; i < invitedList.length; i++){
-    if (invitedList[i] === inputUserText){
+for (let i = 0; i < invitedList.length; i++){
+    if (invitedList[i] == inputUserText){
         checkUserInvitation = true;
     }
 };
 
-if (checkUserInvitation = true) {
+if (checkUserInvitation) {
     console.log("Puoi partecipare al party!")
 } else {
-    console.log("mi dispiace, non puoi partecipare!")};
+    console.log("mi dispiace, non puoi partecipare!")
+};
+
