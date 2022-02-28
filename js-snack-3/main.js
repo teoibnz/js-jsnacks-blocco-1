983bc6f17086d96dcc51322571802be06192a6ff
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-28 15:23:29 +0100

    inserimento commenti nella sezione js

--- js-snack-3/main.js
+++ js-snack-3/main.js
@@ -1,10 +1,13 @@
 
 let sum = 0;
+// con un ciclo creo 10 input dove l'utente dovrà inserire un numero
 
 for (let i= 0; i < 10; i++) {
     const inputNumber = parseInt( prompt("inserisci un numero"));
     console.log(inputNumber);
     sum += inputNumber;
 }
+// alla fine di ogni ciclo viene sommato il numero inserito a quello di partenza
+// alla fine dei cicli verrà mostrata in console la somma di tutti i numeri inseriti
 
-console.log(sum);
+console.log("la somma dei numeri inseriti è " +sum);
