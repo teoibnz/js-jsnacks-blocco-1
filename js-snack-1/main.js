4b6bbcc05cf62982a056ad344777d21c11896872
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-28 15:18:34 +0100

    inserimento commenti e correzioni su es 1 e 2

--- js-snack-1/main.js
+++ js-snack-1/main.js
@@ -1,8 +1,11 @@
+
+// creo due variabili per permettere all'utente di inserire due input numerici
 const firstNumber = prompt("inserisci il primo numero");
 console.log(firstNumber);
 const secondNumber = prompt("Inserisci il secondo numero");
 console.log(secondNumber);
 
+// creo una condizione per la quale verrà stampato nella console una frase che dirà qual'è il numero più grande 
 if (firstNumber > secondNumber) {
     console.log("il primo è il numero superiore")
 } else if (secondNumber > firstNumber) {
--- js-snack-2/main.js
+++ js-snack-2/main.js
@@ -1,15 +1,14 @@
-const firstInputWord = prompt("inserisci la prima parola").length;
-console.log(firstInputWord);
-const secondInputWord = prompt("Inserisci la seconda parola").length;
-console.log(secondInputWord);
+// inserisco due sezioni di input per l'utente dove dovrà inserire due parole
 
-firstInputWord.value ="";
-secondInputWord.value ="";
+const firstInputWord = prompt("inserisci la prima parola");
+
+const secondInputWord = prompt("Inserisci la seconda parola");
+
+
+// successivamente creo una condizione che stamperà in console prima la parola più lunga e dopo quella più corta
 
 if (firstInputWord.length < secondInputWord.length) {
-    console.log("la parola più corta è " +firstInputWord)
-} else if (!firstInputWord.length < secondInputWord.length) {
-    console.log("la parola più corta è " +secondInputWord)
-    } else {
-        console.log('Le parole hanno la stessa lunghezza');
+    console.log('la parola più lunga è ' + secondInputWord + "la parola più corta è  " +firstInputWord)
+} else if (secondInputWord.length < firstInputWord.length) {
+    console.log(firstInputWord + "la parola più corta è " +secondInputWord)
 };
