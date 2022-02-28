852be1c8fb4ce647b77697540979aff4d5eab41b
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-28 17:48:34 +0100

    finalizzazione js-snack 6

--- js-snack-6/main.js
+++ js-snack-6/main.js
@@ -4,9 +4,10 @@ console.log(userNumber);
 
 let sum = 0;
 
-for ( let i = 0; i > userNumber.length; i++) {
+for ( let i = 0; i < userNumber.length; i++) {
     sum += parseInt(userNumber.charAt(i));
-    console.log(sum[i]);
+    
 }; 
 
 
+console.log(sum);
